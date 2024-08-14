d67a562962fe3b0b511aecfa2726f848b44ce482
Gabriela20103967 <20103967@tafe.wa.edu.au>
2024-08-14 11:23:15 +0800

    feat:add inspect helper functions

--- helpers.php
+++ helpers.php
@@ -40,6 +40,30 @@ function loadPartial($name){
     if(file_exists($partialPath)){
         require $partialPath;
     }else {
-        echo "View '{$name} not found!'";
+        echo "Partial '{$name} not found!'";
     }
+}
+
+/**
+ * Inspect a value(s)
+ *
+ *@param mixed $value
+ *@return void
+ */
+function inspect ($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ *
+ *@param mixed $value
+ *@return void
+ */
+function inspectAndDie ($value) {
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
 }
