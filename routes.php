2f7e1d07f3bce965405b1853cb70c3f20633eafc
Gabriela Morales <20103967@tafe.wa.edu.au>
2024-10-02 17:02:50 +0800

    feat:Controller classes - Home & Listings

--- routes.php
+++ routes.php
@@ -3,9 +3,10 @@ use Framework\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
-//$router->get('/', 'controllers/home.php');
-//$router->get('/listings','controllers/listings/index.php');
-//$router->get('/listings/create','controllers/listings/create.php');
-//$router->get('/listing', 'controllers/listings/show.php');
+$router->get('/listings', 'ListingController@index');
+$router->get('/listings/create', 'ListingController@create');
+$router->get('/listing', 'ListingController@show');
+
+
 
 
