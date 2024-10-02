775bbad677211a6268baeebd5c7bd703a226440f
Gabriela Morales <20103967@tafe.wa.edu.au>
2024-10-02 16:40:51 +0800

    feat:Router refactor for controller classes

--- routes.php
+++ routes.php
@@ -2,7 +2,10 @@
 use Framework\Router;
 $router = new Router();
 
-$router->get('/', 'controllers/home.php');
-$router->get('/listings','controllers/listings/index.php');
-$router->get('/listings/create','controllers/listings/create.php');
-$router->get('/listing', 'controllers/listings/show.php');
+$router->get('/', 'HomeController@index');
+//$router->get('/', 'controllers/home.php');
+//$router->get('/listings','controllers/listings/index.php');
+//$router->get('/listings/create','controllers/listings/create.php');
+//$router->get('/listing', 'controllers/listings/show.php');
+
+
