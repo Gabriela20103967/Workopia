98f5cd40d8b904b66fe1a1ffe3a45295020232c4
Gabriela Morales <20103967@tafe.wa.edu.au>
2024-10-06 19:41:44 +0800

    feat:Implement Validation

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -74,7 +74,26 @@ class ListingController{
 
         $newListingData = array_map('sanitize', $newListingData);
 
-        inspectAndDie($newListingData);
+        $requiredFields = ['title', 'description', 'email', 'city', 'state'];
+
+        $errors = [];
+
+        foreach($requiredFields as $field) {
+            if(empty($newListingData[$field]) || !Validation::string($newListingData[$field]))  {
+                $errors[$field] = ucfirst($field).' is required';
+            }
+        }
+
+        if(!empty($errors)) {
+            // Reload view with errors
+            loadView('listings/create', [
+                'errors' => $errors,
+                'listing' => $newListingData
+            ]);
+        }else {
+            // submit data
+            echo 'Success';
+        }
     }
 }
 
