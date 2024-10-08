5107ba556eabd025265b7bf589d3e249d5f54239
Gabriela Morales <20103967@tafe.wa.edu.au>
2024-10-08 13:51:28 +0800

    feat:Delete listings

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -38,6 +38,12 @@ class ListingController{
         loadView('listings/create');
     }
 
+    /**
+     * Show a single listings
+     *
+     * @param array $params
+     * @return void
+     */
     public function show($params) {
         $id = $params['id'] ?? '';
 
@@ -118,6 +124,31 @@ class ListingController{
 
             redirect('/listings');
         }
+    }
+
+    /**
+     * Delete a listing
+     *
+     * @param array $params
+     * @return void
+     */
+    public function destroy($params) {
+        $id = $params['id'];
+
+        $params = [
+          'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        if(!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        $this->db->query('DELETE FROM listings WHERE id = :id', $params);
+
+        redirect('/listings');
     }
 }
 
